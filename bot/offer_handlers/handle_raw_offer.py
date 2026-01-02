53a5fa48f497803b5a907587fc3cfc6e9e71bee6
Logan Sulpizio <logan.sulpizio@widetech.com>
2026-01-02 15:43:42 +0100

    remove test case

--- bot/offer_handlers/handle_raw_offer.py
+++ bot/offer_handlers/handle_raw_offer.py
@@ -28,34 +28,3 @@ def handle_raw_offer(raw_offer: list, DataProperty: dict):
         return None
 
     return offer
-
-
-### test case ###
-if __name__ == '__main__':
-
-    import json
-    from web3 import Web3
-    from web3.exceptions import Web3RPCError
-    from YAM_DB_handlers.get_all_offer_ids_by_seller import get_all_offer_ids_by_seller
-    from bot._contract_data import contract_data
-    from w3_interaction.get_offer import get_multiple_offers
-    import asyncio
-    
-    with open('config.json', 'r', encoding='utf-8') as file:
-        config = json.load(file)
-        db_path = config["db_path"]
-        w3_url = config["w3_url_1"]
-    
-    w3 = Web3(Web3.HTTPProvider(w3_url))
-    
-    offer_ids = get_all_offer_ids_by_seller(db_path, "0xA99e07efB152321117653a16727BF6Bc02106892", ['InProgress'])
-    # Run the get_multiple_offers function
-    results = asyncio.run(get_multiple_offers(w3, offer_ids))
-
-    with open("D:\DeFi\RealtEcosystem\DataProperty.json", 'r', encoding='utf-8') as file:
-        DataProperty = json.load(file)
-
-    for raw_offer in results:
-        offer = handle_raw_offer(raw_offer, DataProperty)
-        from pprint import pprint
-        pprint(offer)
